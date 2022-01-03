66aba4d61392d2b0ccf10a2650d343423bfb76be
tramontana1969 <snegovskojm@gmail.com>
2022-01-03 17:45:22 +0300

    fixed 29_12_2021/task_2.php

--- 29_12_2021/task_2.php
+++ 29_12_2021/task_2.php
@@ -1,10 +1,20 @@
 <?php //https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/php
 function duplicateCount($text) {
-    $arr = str_split(strtolower($text));
-    $counted_values = array_count_values($arr);
+    $arr = [];
+    $counted_values = [];
+    $value_counter = 0;
+    $lowcase = strtolower($text);
+    for ($i = 0; $i < strlen($lowcase); $i++) {
+        if (in_array($lowcase[$i], $arr)) {
+            $value_counter += 1;
+            $counted_values += [$lowcase[$i] => $value_counter];
+        }
+        $arr[] = $lowcase[$i];
+        $value_counter = 0;
+    }
     $counter = 0;
     foreach ($counted_values as $value) {
-        if ($value > 1) {
+        if ($value == 1) {
             $counter += 1;
         }
     }
